705a44a38179033466bfd7f2082788438adaa6ce
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-04-07 02:32:53 +0900

    first router

---
+++ src/views/About.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="about">
+    <p>about</p>
+  </div>
+</template>
---
+++ src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <p>Home</p>
+    <button v-on:click="onclick">トップへ</button>
+  </div>
+</template>
+<script>
+  export default{
+    methods: {
+      onclick(){
+        this.$router.push('about')
+      }
+    }
+  }
+</script>
