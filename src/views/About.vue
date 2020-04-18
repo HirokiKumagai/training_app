e1f1ffc4bb4351b4edc33241840fd23e243437f6
kumagaidiv <hiroki.kumagai@di-v.co.jp>
2020-04-19 02:31:03 +0900

    Aboutコンポーネントの作成
    Vue Router作成に伴うAboutコンポーネントを作成した。

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,15 @@
 <template>
   <div class="about">
     <p>about</p>
+    <button v-on:click="onclick">トップへ</button>
   </div>
 </template>
+<script>
+  export default{
+    methods: {
+      onclick(){
+        this.$router.push('/')
+      }
+    }
+  }
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,13 +2,14 @@
   <div class="home">
     <p>Home</p>
     <button v-on:click="onclick">トップへ</button>
+
   </div>
 </template>
 <script>
   export default{
     methods: {
       onclick(){
-        this.$router.push('about')
+        this.$router.push('/')
       }
     }
   }
